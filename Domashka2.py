f124e50088e3c23c4fbcbbd4638560e7a9913d0b
Skavgarn <slovgarn@mail.ru>
2022-11-19 23:45:03 +0300

    Domashka 2

--- Domashka2.py
+++ Domashka2.py
@@ -1,1 +1,8 @@
-"ksjdbfkhsdfhk"
+import sqlite3
+conn = sqlite3.connect("database.db")
+curs = conn.cursor()
+curs.execute("""CREATE TABLE AUTH(login TEXT, password TEXT);
+""")
+conn.commit()
+curs.close()
+conn.close()
